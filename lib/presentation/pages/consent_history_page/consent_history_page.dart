cefe00382c73603abc2878276efa03da9e106668
Brazhnyk Roman <roman.b@affinidi.com>
2026-08-21 19:09:03 +0800

    refactor: move consent history formatters into presentation layer

--- lib/presentation/pages/consent_history_page/consent_history_page.dart
+++ lib/presentation/pages/consent_history_page/consent_history_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../l10n/app_localizations.dart';
-import '../../../infrastructure/formatters/consent_history_formatters.dart';
+import '../../formatters/consent_history_formatters.dart';
 import '../../themes/app_color_scheme.dart';
 import '../../themes/app_sizing.dart';
 import 'consent_history_details_sheet.dart';
